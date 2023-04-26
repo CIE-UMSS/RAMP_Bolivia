c2eeabce1f218eb212d5a864cd4086cb044c3786
ClaudiaLSS <sanchez.solis.clau@gmail.com>
2023-04-26 13:30:06 +0200

    Deleting refrigerators of Schools Type B

--- inputs/specific_inputs/Specific_loads_lowlands/school_B.py
+++ inputs/specific_inputs/Specific_loads_lowlands/school_B.py
@@ -35,13 +35,6 @@ SB_radio.windows([480,780],[840,1140],0.2)
 SB_DVD = SB.Appliance(SB,2,8,2,120,0.1,5, occasional_use = 0.5)
 SB_DVD.windows([480,780],[840,1140],0.2)
 
-SB_Freezer = SB.Appliance(SB,1,200,1,1440,0,30, 'yes',3)
-SB_Freezer.windows([0,1440])
-SB_Freezer.specific_cycle_1(200,20,5,10)
-SB_Freezer.specific_cycle_2(200,15,5,15)
-SB_Freezer.specific_cycle_3(200,10,5,20)
-SB_Freezer.cycle_behaviour([580,1200],[0,0],[510,579],[0,0],[0,509],[1201,1440])
-
 SB_PC = SB.Appliance(SB,1,50,2,210,0.1,10)
 SB_PC.windows([480,780],[840,1140],0.35)
 
