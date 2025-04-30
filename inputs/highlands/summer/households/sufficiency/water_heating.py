5a00ecd1f5f245ac49a086c08ba651d3d3c89da6
ClaudiaLSS <79788249+ClaudiaLSS@users.noreply.github.com>
2025-04-30 11:40:51 +0200

    Update water heating inputs for highlands, valleys and lowlands.

--- inputs/highlands/summer/households/sufficiency/water_heating.py
+++ inputs/highlands/summer/households/sufficiency/water_heating.py
@@ -1,35 +1,16 @@
 from ramp.core.core import User
-import numpy as np
 import pandas as pd
 
 User_list = []
 
-# === Load daily ambient temperatures from CSV ===
-temp_file_path = "data/Highlands_Daily_temp.csv" # !!!! CHANGE ---------------
-df_temp = pd.read_csv(temp_file_path)
-daily_ambient_temp = df_temp["Temperature"].values
 
-daily_ambient_temp_K = daily_ambient_temp + 273.15
-annual_mean_temp_K = np.mean(daily_ambient_temp_K)
-coldest_day = int(np.argmin(daily_ambient_temp)) + 1
+HWH = User("household water heating", 1)  # Create user with ID = 1
+User_list.append(HWH)
 
-# === Physical parameters for water heating ===
-cp_water = 4.18         # kJ/kg·K - specific heat of water
-mass_flow = 0.08        # kg/s - water flow rate for a shower
-target_temp = 40        # °C - desired hot water temperature
+HH_shower_P = pd.read_csv("data/HL_power_profile_water_heating.csv")
 
-# === Calculate groundwater temperature ===
-days = np.arange(1, 366)
-groundwater_temp_K = daily_ambient_temp_K - 3 * np.cos((2 * np.pi / 365) * (days - coldest_day))
-groundwater_temp_C = groundwater_temp_K - 273.15
 
-# === Thermal power required to heat water, per day (in watts) ===
-power_profile_W = mass_flow * cp_water * (target_temp - groundwater_temp_C) * 1000
-
-HH = User("household hygiene", 1)  # Create user with ID = 1
-User_list.append(HH)
-
-HH_shower = HH.add_appliance(1, power_profile_W, 2, 30, 0.2, 3, thermal_P_var = 0.4)
-HH_shower.windows([360, 540], [1080, 1200], 0.2)
+HWH_shower = HWH.add_appliance(1, HH_shower_P, 2, 30, 0.2, 3, thermal_P_var = 0.4)
+HWH_shower.windows([360, 540], [1080, 1200], 0.2)
 
 
--- inputs/lowlands/fall/households/sufficiency/water_heating.py
+++ inputs/lowlands/fall/households/sufficiency/water_heating.py
@@ -7,11 +7,14 @@ Created on Wed Apr 23 15:42:41 2025
 """
 
 from ramp.core.core import User
+import pandas as pd
 
 User_list = []
 
-HH = User("household hygiene", 1)
-User_list.append(HH)
+HWH = User("household water heating", 1)  # Create user with ID = 1
+User_list.append(HWH)
 
-HH_shower = HH.add_appliance(1,5500,2,40,0.1,3, thermal_P_var = 0.2)
-HH_shower.windows([360,540],[1080,1260],0.2)
+HH_shower_P = pd.read_csv("data/LL_power_profile_water_heating.csv")
+
+HWH_shower = HWH.add_appliance(1,5500,2,40,0.1,3, thermal_P_var = 0.2)
+HWH_shower.windows([360,540],[1080,1260],0.2)
