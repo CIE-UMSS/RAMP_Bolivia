40dbb8041f7b4c5b3b644037ec08537e35e4d7ca
rogerariass <rogerarias2001@gmail.com>
2025-05-14 19:48:57 +0200

    Cold storage errors resolved

--- inputs/highlands/winter/community_services/health_center/cold_storage.py
+++ inputs/highlands/winter/community_services/health_center/cold_storage.py
@@ -10,6 +10,4 @@ CS_Freezer = CS.add_appliance(1, 200, 1, 1440, 0, 30, "yes", 2)
 CS_Freezer.windows([0, 1440], [0, 0])
 CS_Freezer.specific_cycle_1(200, 15, 5, 15) #intemedio
 CS_Freezer.specific_cycle_2(200, 10, 5, 20) #standard
-CS_Freezer.cycle_behaviour(
-    [330, 1139], [0, 0], [0, 329], [1140, 1440]
-)
+CS_Freezer.cycle_behaviour([330, 1139], [0, 0], [0, 329], [1140, 1440])
